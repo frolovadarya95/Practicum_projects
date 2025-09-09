ef0903471dd2ad3bbe25460c10ec753a6b001687
Дарья Фролова <daschafobos95@yandex.ru>
2025-09-09 13:42:46 +0300

    Аналитика данных онлайн-игры.sql

--- SQL/sql вариант_3 (1).sql
+++ SQL/sql вариант_3 (1).sql
@@ -12,117 +12,117 @@
 
 -- 1.1. Доля платящих пользователей по всем данным:
 
---WITH count AS (
---SELECT 
---COUNT(id) AS count_id, (
-	--SELECT COUNT(payer) AS count_payer
-	--FROM fantasy.users
-	--WHERE payer=1
-	--)
---FROM fantasy.users
---)
---SELECT *,
---ROUND((count_payer::numeric/count_id),2) AS share
---FROM count;
+WITH count AS (
+SELECT 
+COUNT(id) AS count_id, (
+	SELECT COUNT(payer) AS count_payer
+	FROM fantasy.users
+	WHERE payer=1
+	)
+FROM fantasy.users
+)
+SELECT *,
+ROUND((count_payer::numeric/count_id),2) AS share
+FROM count;
 
 -- 1.2. Доля платящих пользователей в разрезе расы персонажа:
 
---WITH table_1 AS (
---SELECT 
---race,
---COUNT(payer) AS count_payer
---FROM fantasy.users
---JOIN fantasy.race USING(race_id)
---WHERE payer=1
---GROUP BY race
---),
---table_2 AS (
---SELECT 
---race,
---COUNT(id) AS count_id
---FROM fantasy.users
---JOIN fantasy.race USING(race_id)
---GROUP BY race
---)
---SELECT *,
---ROUND((count_payer::numeric/count_id),2) AS share
---FROM table_1
---JOIN table_2 USING (race);
+WITH table_1 AS (
+SELECT 
+race,
+COUNT(payer) AS count_payer
+FROM fantasy.users
+JOIN fantasy.race USING(race_id)
+WHERE payer=1
+GROUP BY race
+),
+table_2 AS (
+SELECT 
+race,
+COUNT(id) AS count_id
+FROM fantasy.users
+JOIN fantasy.race USING(race_id)
+GROUP BY race
+)
+SELECT *,
+ROUND((count_payer::numeric/count_id),2) AS share
+FROM table_1
+JOIN table_2 USING (race);
 
 -- Задача 2. Исследование внутриигровых покупок
 -- 2.1. Статистические показатели по полю amount:
 
---SELECT 
---COUNT(transaction_id ) AS count_id,
---SUM(amount) AS sum_amount,
---MIN(amount) AS min_amount,
---MAX(amount) AS max_amount,
---AVG(amount) AS avg_amount,
---PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY amount) AS median,
---STDDEV(amount) AS stand_dev
---FROM fantasy.events;
+SELECT 
+COUNT(transaction_id ) AS count_id,
+SUM(amount) AS sum_amount,
+MIN(amount) AS min_amount,
+MAX(amount) AS max_amount,
+AVG(amount) AS avg_amount,
+PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY amount) AS median,
+STDDEV(amount) AS stand_dev
+FROM fantasy.events;
 
 -- 2.2: Аномальные нулевые покупки:
 
---WITH table_1 AS (
---SELECT 
---COUNT(transaction_id) AS count_id, (
---SELECT
---COUNT(amount) AS count_amount
---FROM fantasy.events
---WHERE amount=0
---)
---FROM fantasy.events
---)
---SELECT *,
---count_amount::NUMERIC/count_id AS share
---FROM table_1;
+WITH table_1 AS (
+SELECT 
+COUNT(transaction_id) AS count_id, (
+SELECT
+COUNT(amount) AS count_amount
+FROM fantasy.events
+WHERE amount=0
+)
+FROM fantasy.events
+)
+SELECT *,
+count_amount::NUMERIC/count_id AS share
+FROM table_1;
 
 -- 2.3: Сравнительный анализ активности платящих и неплатящих игроков:
 
---SELECT 
---CASE
-	--WHEN u.payer=1
-	--THEN 'Платящие'
-	--ELSE 'Неплатящие'
---END AS catecory,
---COUNT(DISTINCT u.id) AS kol_vo_igrokov, --общее количество игроков
---COUNT(e.transaction_id)/COUNT(DISTINCT u.id) AS avg_cl_pokupok, -- среднее количество покупок
---ROUND(SUM(e.amount)::numeric/COUNT(DISTINCT u.id),2) AS avg_ct_pokupok -- среднюю суммарную стоимость покупок на одного игрока
---FROM fantasy.users AS u
---LEFT JOIN fantasy.events AS e ON u.id=e.id
---WHERE e.amount>0 
---GROUP BY catecory;
+SELECT 
+CASE
+	WHEN u.payer=1
+	THEN 'Платящие'
+	ELSE 'Неплатящие'
+END AS catecory,
+COUNT(DISTINCT u.id) AS kol_vo_igrokov, --общее количество игроков
+COUNT(e.transaction_id)/COUNT(DISTINCT u.id) AS avg_cl_pokupok, -- среднее количество покупок
+ROUND(SUM(e.amount)::numeric/COUNT(DISTINCT u.id),2) AS avg_ct_pokupok -- среднюю суммарную стоимость покупок на одного игрока
+FROM fantasy.users AS u
+LEFT JOIN fantasy.events AS e ON u.id=e.id
+WHERE e.amount>0 
+GROUP BY catecory;
 
 -- 2.4: Популярные эпические предметы:
 
---WITH table_1 AS (
---SELECT i.game_items, --выделяем группы эпических предметов 
---COUNT(DISTINCT e.id) AS kolvo_igrokov, -- считаем кол-во уникальных игроков
---COUNT(e.amount) AS absolut_kolvo_pokupok -- считаем кол-во абсолютное кол-во покупок
---FROM fantasy.items AS i
---LEFT JOIN fantasy.events AS e ON i.item_code=e.item_code
---WHERE e.amount>0
---GROUP BY i.game_items
---),
---table_2 AS (
---SELECT *,
---SUM(absolut_kolvo_pokupok) OVER () AS obshee_kolvo_pokupok --считаем общее кол-во покупок
---FROM table_1
---),
---table_3 AS (
---SELECT *,
---absolut_kolvo_pokupok/obshee_kolvo_pokupok AS otnosit_kolvo_pokupok, -- считаем относительное кол-во покупок
---kolvo_igrokov::numeric/(SELECT COUNT(DISTINCT id) FROM fantasy.events WHERE amount>0) AS doly_igrocov  -- считаем долю игроков от общего кол-ва покупок
---FROM table_2
---)
---SELECT 
---game_items,
---absolut_kolvo_pokupok,
---otnosit_kolvo_pokupok,
---doly_igrocov
---FROM table_3
---ORDER BY absolut_kolvo_pokupok DESC;
+WITH table_1 AS (
+SELECT i.game_items, --выделяем группы эпических предметов 
+COUNT(DISTINCT e.id) AS kolvo_igrokov, -- считаем кол-во уникальных игроков
+COUNT(e.amount) AS absolut_kolvo_pokupok -- считаем кол-во абсолютное кол-во покупок
+FROM fantasy.items AS i
+LEFT JOIN fantasy.events AS e ON i.item_code=e.item_code
+WHERE e.amount>0
+GROUP BY i.game_items
+),
+table_2 AS (
+SELECT *,
+SUM(absolut_kolvo_pokupok) OVER () AS obshee_kolvo_pokupok --считаем общее кол-во покупок
+FROM table_1
+),
+table_3 AS (
+SELECT *,
+absolut_kolvo_pokupok/obshee_kolvo_pokupok AS otnosit_kolvo_pokupok, -- считаем относительное кол-во покупок
+kolvo_igrokov::numeric/(SELECT COUNT(DISTINCT id) FROM fantasy.events WHERE amount>0) AS doly_igrocov  -- считаем долю игроков от общего кол-ва покупок
+FROM table_2
+)
+SELECT 
+game_items,
+absolut_kolvo_pokupok,
+otnosit_kolvo_pokupok,
+doly_igrocov
+FROM table_3
+ORDER BY absolut_kolvo_pokupok DESC;
 
 -- Часть 2. Решение ad hoc-задач
 -- Задача 1. Зависимость активности игроков от расы персонажа:
@@ -168,9 +168,8 @@ ROUND(sum_amount::numeric/count_id_amount,2) AS avg_sum_amount -- средняя
 FROM table_1
 JOIN table_2 USING (race)
 JOIN table_3 USING (race)
-GROUP BY race, count_id, count_id_amount, count_transaction_id, sum_amount, fraction_amount
--- Задача 2: Частота покупок
--- Напишите ваш запрос здесь
+GROUP BY race, count_id, count_id_amount, count_transaction_id, sum_amount, fraction_amount;
+
 
 
 
